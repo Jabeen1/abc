90b58fdb3924097ad55fed68ae3145dcec290a15
ismaeel <ismaeel@10.0.0.61>
2018-11-06 17:30:01 -0500

    first commit uploading all files

---
+++ GetInterviewApplication/src/main/java/com/get/interview/GetInterviewApplication.java
@@ -0,0 +1,14 @@
+package com.get.interview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetInterviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetInterviewApplication.class, args);
+		
+		System.out.println("Welcome to boot");
+	}
+}
